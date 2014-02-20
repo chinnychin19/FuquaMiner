85270981a03e5133df816c007543a67df731df73
Chinmay Patwardhan <chinnychin19@gmail.com>
2014-02-20 02:31:01 -0500

    added more fields to be saved and implemented stuff to avoid hitting the rate limit

--- Twitter/app.js
+++ Twitter/app.js
@@ -4,7 +4,7 @@ var csv = require('./csv.js');
 var bigInt = require('big-integer');
 var fs = require('fs');
 
-var T = new Twit(config.credentials);
+var TScraper = new Twit(config.credentials);
 
 var TIME_SECOND = 1000;
 var TIME_MINUTE = 60 * TIME_SECOND;
@@ -19,28 +19,66 @@ var keyObj = {
 	"created_at":null,
 	"retweet_count":null,
 	"favorite_count":null,
+	"truncated":null,
+	"in_reply_to_user_id_str":null,
+	"in_reply_to_status_id_str":null,
+	"geo":null,
+	"possibly_sensitive":null,
+	"place":null,
+	"in_reply_to_screen_name":null,
+	"source":null,
 }
 
-var DEFAULT_NUM_TWEETS = 190;
+var DEFAULT_NUM_TWEETS = 100000;
+
+var RATE_LIMIT = 3;
+var RATE_PERIOD = 3*TIME_SECOND;
 
 var args = process.argv.slice(2);
 var numTweets = args.indexOf('-n') >= 0 ? Number(args[args.indexOf('-n') + 1]) : DEFAULT_NUM_TWEETS;
 var DEBUG_MODE = args.indexOf('-d') >= 0;
 
+var requestQueue = []; //stores the relevant data to be submitted in each tweet
+readScreenNames('./screen_names.in');
+setInterval(mainFunction, RATE_PERIOD);
+
+// When called, submits at most RATE_LIMIT requests from the request queue
+function mainFunction() {
+	console.log('calling main');
+	var limit = RATE_LIMIT;
+	while (requestQueue.length > 0 && limit > 0) {
+		var requestObject = requestQueue.shift();
+		limit--;
+		console.log("sending request for "+requestObject.options.screen_name);
+		TScraper.get(requestObject.request, requestObject.options, requestObject.callback);
+	}
+	if (requestQueue.length == 0) {
+		console.log("All requests sent");
+	} else {
+		console.log("Pending requests in queue: "+requestQueue.length);
+	}
+}
+
+// Only prints to console when executed with -d option (debug)
 function log(str) {
 	if (DEBUG_MODE) {
 		console.log(str);
 	}
 }
 
-fs.readFile('./screen_names.in', function(err, data) {
-	var screen_names = data.toString().trim().split('\n');
-	screen_names.forEach(function(element) {
-		log(element.trim());
-		scrapeAllTweets(element.trim(), numTweets);
+// Reads all lines from an input file
+// Each line consists of a screen name
+// Calls a helper function to scrape all tweets of each screen name
+function readScreenNames(filename) {
+	fs.readFile(filename, function(err, data) {
+		var screen_names = data.toString().trim().split('\n');
+		screen_names.forEach(function(element) {
+			var screen_name = element.trim();
+			log(screen_name);
+			scrapeAllTweets(screen_name, numTweets);
+		});
 	});
-});
-
+}
 
 
 function scrapeAllTweets(screen_name, numTweets) {
@@ -56,9 +94,10 @@ function scrapeAllTweets_helper(screen_name, outfile, numTweets, maxTweetId) {
 		options.max_id = maxTweetId;
 	}
 
-	T.get("statuses/user_timeline", 
-		options,
-		function(err, reply) {
+	var requestObject = {};
+	requestObject.request = "statuses/user_timeline";
+	requestObject.options = options;
+	requestObject.callback = function(err, reply) {
 			if (err) {
 				throw err;
 			}
@@ -75,7 +114,7 @@ function scrapeAllTweets_helper(screen_name, outfile, numTweets, maxTweetId) {
 				log('Done! ('+screen_name+')');
 			}
 		}
-	);
+	requestQueue.push(requestObject);
 }
 
 // Just 3 methods to access the full twitter API.
