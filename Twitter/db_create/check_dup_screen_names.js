b474f0ef7177f61317d2ff4c6b3dad981ca6e662
Chinmay Patwardhan <chinnychin19@gmail.com>
2014-04-11 15:45:46 -0400

    updated screen names

--- Twitter/db_create/check_dup_screen_names.js
+++ Twitter/db_create/check_dup_screen_names.js
@@ -5,18 +5,18 @@ fs.readFile('../screen_names.in', function(err, data) {
 	var screen_names = data.toString().trim().split('\n');
 	var set = {};
 	screen_names.forEach(function(element) {
-		var screen_name = element.trim();
+		var screen_name = element.trim().toLowerCase();
 		if (set[screen_name]) {
-			console.log(screen_name);
+			console.log("DUP: "+screen_name);
 		}
 		set[screen_name] = true;
 	});
 	fs.readFile('../screen_names_already_scraped.in', function(err, data) {
 		var screen_names = data.toString().trim().split('\n');
 		screen_names.forEach(function(element) {
-			var screen_name = element.trim();
+			var screen_name = element.trim().toLowerCase();
 			if (set[screen_name]) {
-				console.log(screen_name);
+				console.log("DUP: "+screen_name);
 			}
 			set[screen_name] = true;
 		});
